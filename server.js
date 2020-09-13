3ce95c84b8ce4c0681dc75cd52474b94b48257f9
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 23:05:29 -0700

    Setup PORT, app express and handlebars

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+// Dependencies
 const express = require('express');
-
+// Setup PORT
 const PORT = process.env.PORT || 8080;
-
+// Setup app express
 const app = express();
 
+// Serve static content for the app from the "public"
+// directory in the application directory.
+app.use(express.static('public'));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
