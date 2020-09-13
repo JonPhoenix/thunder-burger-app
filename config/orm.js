4f49e5e07353c70d8fa87d8328f997660c930a67
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 21:40:11 -0700

    Adding update property to orm.js

--- config/orm.js
+++ config/orm.js
@@ -68,6 +68,24 @@ const orm = {
             cb(result);
         });
     },
+    
+    update: (table, objColVals, condition, cb) => {
+        let queryString = `UPDATE ${table}`;
+
+        queryString += ` SET `;
+        queryString += objToSql(objColVals);
+        queryString += ` WHERE `;
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
 };
 
 // Export the orm object for the model.
