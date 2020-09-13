13f868fba056ccf6a844a32e8b1a88b95c160d2d
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 21:58:55 -0700

    Adding select all method to burger.js model

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require('../config/orm.js');
 
+// Methods to call the orm functions using burger input for orm.
 const burger = {
     selectAll: cb => {
         orm.all('burgers', res => {
