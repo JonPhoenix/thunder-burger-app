574f59e735f53a5dcb53847f26efa87a87d9a06f
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 23:08:16 -0700

    Import routes and give server.js access to them

--- server.js
+++ server.js
@@ -18,3 +18,8 @@ const exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
