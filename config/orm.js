5f268ec8b7981a100af8dab3a93a4fa7a70f0afe
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 21:39:09 -0700

    Adding create property to orm.js

--- config/orm.js
+++ config/orm.js
@@ -47,6 +47,27 @@ const orm = {
             cb(result);
         });
     },
+
+    create: (table, cols, vals, cb) => {
+        let queryString = 'INSERT INTO ' + table;
+    
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+    
+            cb(result);
+        });
+    },
 };
 
 // Export the orm object for the model.
