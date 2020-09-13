a14e344061564796deeff2e00a678f33958a3ac6
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 21:34:39 -0700

    Adding select all method to orm.js

--- config/orm.js
+++ config/orm.js
@@ -36,6 +36,18 @@ const objToSql = ob => {
 }
 
 // Object for all our SQL statement functions.
+const orm = {
+
+    all: (tableInput, cb) => {
+        const queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+};
 
 // Export the orm object for the model.
 module.exports = orm;
