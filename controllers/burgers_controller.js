389574e4c8f096a5fe05d9f3f75d718fb68a5d99
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 22:57:27 -0700

    Creating router delete - delete one

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,7 +29,7 @@ router.post('/api/burgers', (req, res) => {
     });
 });
 
-router.put("/api/burgers/:id", (req, res) => {
+router.put('/api/burgers/:id', (req, res) => {
     let condition = `id = ${req.params.id}`;
 
     console.log('condition', condition);
@@ -41,10 +41,28 @@ router.put("/api/burgers/:id", (req, res) => {
         condition,
         result => {
             if (result.changedRows === 0) {
-                // If no rows were changed, then the ID must not exist, so 404
+                // If no rows were changed, 
+                // then the ID must not exist, so 404.
                 return res.status(404).end();
             }
             else {
+                res.status(200).end();
+            }
+        }
+    );
+});
+
+router.delete('/api/burgers/:id', (req, res) => {
+    let condition = `id = ${req.params.id}`;
+
+    burger.deleteOne(
+        condition, 
+        result => {
+            if (result.affectedRows == 0) {
+                // If no rows were changed, 
+                // then the ID must not exist, so 404.
+                return res.status(404).end();
+            } else {
                 res.status(200).end();
             }
         }
