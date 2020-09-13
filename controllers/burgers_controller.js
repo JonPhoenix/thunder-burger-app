d155e70256e34d45b7991f818596af64281b971f
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 22:33:17 -0700

    Creating router get - select all

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,19 @@
+// Import express
 const express = require('express');
 
+// Create the router for the app
+const router =  express.Router();
+
+// Import the model burger.js to use its database functions.
 const burger =  require('../models/burger.js');
+
+// Create all routes and setup logic within where required.
+router.get("/", (req, res) => {
+    burger.selectAll(data => {
+      const hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
