c2446664d637c5e28576f06b0a3669584df4798a
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 22:48:38 -0700

    Creating router put - update one

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,13 +10,13 @@ const burger =  require('../models/burger.js');
 // Create all routes and setup logic within where required.
 router.get('/', (req, res) => {
     burger.selectAll(data => {
-      const hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render('index', hbsObject);
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
     });
-  });
+});
 
 router.post('/api/burgers', (req, res) => {
     burger.insertOne([
@@ -27,4 +27,26 @@ router.post('/api/burgers', (req, res) => {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    let condition = `id = ${req.params.id}`;
+
+    console.log('condition', condition);
+
+    burger.updateOne(
+        {
+          devoured: 1
+        },
+        condition,
+        result => {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            else {
+                res.status(200).end();
+            }
+        }
+    );
 });
