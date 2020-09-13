c00502017763f102bcb20ea81b96fe0d2f69e420
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 20:08:35 -0700

    Create connection details

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,22 @@
 // Setup MySQL connection 
 const mysql = require('mysql');
 
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "tburgers_db"
+  });
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
 // Module.export connection to orm.js
 module.export = connection;
