6f9ad88ba57a9ed31c7dff0a8ab0cf8e802e1b83
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-12 22:39:10 -0700

    Creating router post - insert one

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,12 +8,23 @@ const router =  express.Router();
 const burger =  require('../models/burger.js');
 
 // Create all routes and setup logic within where required.
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     burger.selectAll(data => {
       const hbsObject = {
         burgers: data
       };
       console.log(hbsObject);
-      res.render("index", hbsObject);
+      res.render('index', hbsObject);
     });
   });
+
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne([
+        'burger_name', 'devoured'
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], result => {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+});
